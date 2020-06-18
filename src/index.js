e0f5aafffcbb46c5722bf53e451802eb58fcc549
Aravind A <aravindarali125@gmail.com>
2020-06-19 02:24:58 +0530

    Made some adjustments in package.json and added dev port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const taskRouter = require("./routers/task");
 
 
 const app =express();
-const port=process.env.PORT;
+const port=process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter);
