8827defd0d7c1c766d707c93b9ec7861fea23cdd
Aravind A <aravindarali125@gmail.com>
2020-06-19 01:08:07 +0530

    Updated comments regarding environmental variables

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 const auth = async (req,res,next)=>{
     try{
         const token = req.header("Authorization").replace("Bearer ",'');
-        const decoded =jwt.verify(token,process.env.jwt_secret);
+        const decoded =jwt.verify(token,process.env.jwt_secret); // your jwt token secret key
         const user =await User.find({_id:decoded._id},{"tokens.token":token}).select("name age email password");
         if(!user){
             throw new Error();
--- src/models/user.js
+++ src/models/user.js
@@ -66,7 +66,7 @@ userSchema.methods.toJSON = function(){
 
 userSchema.methods.generateAuthToken = async function(){
     const user = this;
-    const token = jwt.sign({_id:user._id.toString()},process.env.jwt_secret);
+    const token = jwt.sign({_id:user._id.toString()},process.env.jwt_secret); // your jwt token secret key
     user.tokens = user.tokens.concat({token});
     await user.save();
     return token;
